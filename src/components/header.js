0b30fb35b69c5bd6fc03d9806d6ee623404b6f39
Peng Xue Vang <pxvang33@gmail.com>
2019-06-13 16:46:28 -0500

    header component

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+
+
+
+class Header extends Component {
+    // render is a function that will get called when your component gets called
+    // render needed to visually display something on screen
+    render(){
+        // return something to render
+        return (
+            <header>
+                <h1>We building React!</h1>
+            </header>
+        );
+    }
+
+}
+export default Header;
